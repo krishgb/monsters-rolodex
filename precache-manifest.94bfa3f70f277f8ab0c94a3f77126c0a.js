7fdc622dddccfe42e374be0e267e8a39597963bd
krish <krish@email.com>
2020-09-22 07:55:48 +0530

    Updates

---
+++ precache-manifest.94bfa3f70f277f8ab0c94a3f77126c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51be632daa951f9f9bb6b48bd49de822",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ea7bb8d55026dac00e9",
+    "url": "/monsters-rolodex/static/css/main.66e58abf.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea7bb8d55026dac00e9",
+    "url": "/monsters-rolodex/static/js/main.93724f14.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
